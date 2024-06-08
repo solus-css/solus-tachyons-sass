9da93eb24ebd5d5ef9c8abc1a22bee4c1ae69ff0
Benjamin Rush <bnjmnrsh@gmail.com>
2024-06-08 10:13:48 +0100

    update deps, remove depricated packages Summary: stylelint-config-prettier-scss which relies on stylelint-config-prettier has been depricated as of Stylelint v15 all style-related rules have been deprecated.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"], // ,
+  extends: ['stylelint-config-standard-scss'], // ,
   rules: {
     // recommended rules
-    "at-rule-no-unknown": null,
-    "comment-empty-line-before": null,
-    "scss/at-rule-no-unknown": true,
-    "scss/at-mixin-pattern": null,
-  },
-};
+    'at-rule-no-unknown': null,
+    'comment-empty-line-before': null,
+    'scss/at-rule-no-unknown': true,
+    'scss/at-mixin-pattern': null
+  }
+}
