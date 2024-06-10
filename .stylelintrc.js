91277242e7e309ad35ca275edfa72a6813cee94e
Benjamin Rush <bnjmnrsh@gmail.com>
2024-06-10 01:25:51 +0100

    Sligthly more nuance in formatting line breaks.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,23 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss'], // ,
+  extends: ["stylelint-config-standard-scss"],
+  plugin: ["stylelint-prettier"],
   rules: {
     // recommended rules
-    'at-rule-no-unknown': null,
-    'comment-empty-line-before': null,
-    'scss/at-rule-no-unknown': true,
-    'scss/at-mixin-pattern': null
-  }
-}
+    "at-rule-no-unknown": null,
+    "comment-empty-line-before": null,
+    "scss/at-rule-no-unknown": true,
+    "scss/at-mixin-pattern": null,
+    "at-rule-empty-line-before": [
+      "never",
+      {
+        ignore: ["after-comment", "first-nested"],
+        except: [
+          "after-same-name",
+          "blockless-after-blockless",
+          "blockless-after-same-name-blockless",
+          "inside-block",
+        ],
+      },
+    ],
+  },
+};
